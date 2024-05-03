1563a46f5be3c4e66f79242508d76010fefc12bc
hoffeer <168199852+hoffeer@users.noreply.github.com>
2024-05-03 16:59:03 -0300

    TP2 - Contabilidad.py

--- TP2 - Contabilidad.py
+++ TP2 - Contabilidad.py
@@ -4,12 +4,17 @@ os.system("cls")
 b=0;
 c=0;
 
-for x in range (2):
+for x in range (10):
     x=int(input("Ingrese un numero: "))
 
-    if x%3 == 0:
+    if x%3 == 0 and x%5 == 0:
         b=b+1
-    if x%5 == 0:
         c=c+1
+    elif x%3 == 0:
+        b=b+1
+    elif x%5 == 0:
+        c=c+1
+    else:
+        print("No hay multiplos de 3 o 5")
 print("La cantidad de multiplos de 3 son: ",b)
 print("La cantidad de multiplos de 5 son: ",c)
